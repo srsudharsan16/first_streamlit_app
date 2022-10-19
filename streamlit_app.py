4d500d3ea8150cf131a9549504309a1a84e9e6d1
srsudharsan16 <116171529+srsudharsan16@users.noreply.github.com>
2022-10-19 20:59:06 +0800

    Update streamlit_app.py
    Added pandas, brought in the text file from S3 and added it here.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.text('🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('hard Boiled Fee-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dtaframe(my_fruit_list)
