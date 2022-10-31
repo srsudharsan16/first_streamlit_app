cf2222ec2e80e11ca2165f749752e572f384f04c
srsudharsan16 <116171529+srsudharsan16@users.noreply.github.com>
2022-10-31 18:50:10 +0800

    Added fruityice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+import requests
+
+fruityvice_response = requests.get("https://fruityice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
