213456fea208b2d851472fce0c40af6e32acc8c3
srsudharsan16 <116171529+srsudharsan16@users.noreply.github.com>
2022-10-31 18:52:49 +0800

    Fruityvice_response typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 
-fruityvice_response = requests.get("https://fruityice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
