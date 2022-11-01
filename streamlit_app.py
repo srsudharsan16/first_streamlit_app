bb6705a8c1ee06702da0003fbb192e040bded18f
srsudharsan16 <116171529+srsudharsan16@users.noreply.github.com>
2022-11-01 19:34:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text('🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('hard Boiled Fee-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+import snowflake.connector
 
 
 import pandas
